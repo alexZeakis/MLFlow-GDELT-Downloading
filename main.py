782540becc75fed63dc4835fad179c628b33f841
alexZeakis <alzeakis@gmail.com>
2023-06-01 11:38:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,77 +4,8 @@ import pandas as pd
 import urllib.request
 from time import time       
 import json 
+from utils import make_dataset
 
-names = ['GKGRECORDID', 'DATE', 'SOURCECOLLECTIONIDENTIFIER',
-         'SOURCECOMMONNAME', 'DOCUMENTIDENTIFIER', 'COUNTS', 'COUNTS_2',
-         'THEMES', 'ENHANCEDTHEMES', 'LOCATIONS', 'ENHANCEDLOCATIONS',
-         'PERSONS', 'ENHANCEDPERSONS', 'ORGANIZATIONS',
-         'ENHANCEDORGANIZATIONS', '5TONE', 'ENHANCEDDATES', 'GCAM',
-         'SHARINGIMAGE', 'RELATEDIMAGES', 'SOCIALIMAGEEMBEDS',
-         'SOCIALVIDEOEMBEDS', 'QUOTATIONS', 'ALLNAMES', 'AMOUNTS',
-         'TRANSLATIONINFO', 'EXTRASXML']
-
-sel_names = ['GKGRECORDID', 'DATE', 'SOURCECOMMONNAME', 'DOCUMENTIDENTIFIER',
-             'THEMES', 'LOCATIONS', 'PERSONS', 'ORGANIZATIONS', '5TONE']
-
-def download_list(prefix, out):
-    df_files = pd.read_csv(f'{out}temp/{prefix}/masterfilelist.txt', header=None, sep=' ')
-    df_files[2] = df_files[2].fillna('')
-    df_files = df_files.loc[df_files[2].apply(lambda x: 'gkg' in x)]
-
-    files = pd.Series([file for file in df_files[2] if prefix in file])
-    # files = files.head(2)
-    
-    for file in files:
-        out_file = file.replace('http://data.gdeltproject.org/gdeltv2/', '')
-        if not os.path.exists(f"{out}temp/{prefix}/{out_file}"):
-            print(f'Downloading {file}\r', end='')
-            urllib.request.urlretrieve(file, 
-                                       f"{out}temp/{prefix}/{out_file}")
-    
-def filter_datasets(prefix, out):
-    out_file = f'{out}csv/{prefix}_filtered.csv'
-    
-    if os.path.exists(out_file):
-        return {}
-    
-    themes = set(pd.read_csv('./agro_themes.csv', header=None).apply(lambda x: x[0], axis=1).values)
-    original_no_articles = 0
-    filtered_no_articles = 0
-    with open(out_file, 'w') as o:
-        for file in os.listdir(f'{out}temp/{prefix}'):
-            try:
-                if not file.endswith(".zip"):
-                    continue
-                print(f'Filtering {file}\r', end='')
-                df = pd.read_csv(f'{out}temp/{prefix}/{file}', sep='\t', names=names, usecols=sel_names, encoding="ISO-8859-1")
-                
-                original_no_articles += df.shape[0]
-                df = df.loc[df['THEMES'].fillna('').apply(lambda x: len(set(x.split(';')) & themes) > 0)]
-                filtered_no_articles += df.shape[0]
-                df.to_csv(o, header=None, index=None)
-            except:
-                continue
-            
-    return {'original_no_articles': original_no_articles, 
-            'filtered_no_articles': filtered_no_articles}
-
-    
-def make_dataset(prefix, out):
-    csv_file = f'{out}csv/{prefix}_filtered.csv'
-    
-    if not os.path.exists(csv_file):
-        print(f'Creating {csv_file}')
-        if not os.path.exists(f'{out}temp/{prefix}'):
-            os.mkdir(f'{out}temp/{prefix}')
-            
-        if not os.path.exists(f'{out}temp/{prefix}/masterfilelist.txt'):    
-            urllib.request.urlretrieve("http://data.gdeltproject.org/gdeltv2/masterfilelist.txt", 
-                                   f"{out}temp/{prefix}/masterfilelist.txt")   
-            
-        download_list(prefix, out)
-        return filter_datasets(prefix, out)
-        # os.rmdir(f'{out}temp/{prefix}')    
 
 if __name__ == '__main__':
     print(sys.argv)
--- run_project.py
+++ run_project.py
@@ -2,38 +2,60 @@ import mlflow
 import argparse
 import json
 
-parser = argparse.ArgumentParser(description='An mlflow project for downloading \
-                                 GDELT articles from specific date.')
-
-parser.add_argument('-d', '--date', required=True, help='Date to download GDELT articles in the form of YYYYMMDD.')
-parser.add_argument('-e', '--experiment', required=True, help='Experiment Name of MLFlow')
-parser.add_argument('-a', '--airflow', help='Airflow run_id for connection')
-parser.add_argument('-o', '--output', default="../data/", help='Path to the output file.')
-parser.add_argument('-l', '--log', default="../logs/ingest/", help='Path to the log file.')
-parser.add_argument('-t', '--tracking', help='URL of MLFlow tracking server')
-parser.add_argument('-u', '--user', help='Name of MLFlow User.')
-
-args = parser.parse_args()
-
-if args.tracking is not None:
-    mlflow.set_tracking_uri(args.tracking)
-if args.user is not None:    
-    mlflow.set_tag("user", args.user)
-
-log_file = args.log+args.date+'.json'
-
-# Set the parameters
-parameters = {"date": args.date, "out": args.output, "logs": log_file,
-              "airflow": args.airflow}
-
-# Run the project
-info = mlflow.run('./', parameters=parameters, experiment_name=args.experiment)
-mlflow.end_run()
-
-with open(log_file) as o:
-    j = json.load(o)
-    mlflow.start_run(run_id=info.run_id)
-    for key, val in j.items():
-        mlflow.log_metric(key, val)
+def run(**kwargs):
+    
+    date = kwargs.get('date')
+    experiment = kwargs.get('experiment')
+    airflow = kwargs.get('airflow')
+    output = kwargs.get('output')
+    log = kwargs.get('log')
+    tracking = kwargs.get('tracking')
+    user = kwargs.get('user')
+    
+    if tracking is not None:
+        mlflow.set_tracking_uri(tracking)
+    if user is not None:    
+        mlflow.set_tag("user", user)
+    
+    log_file = log+date+'.json'
+    
+    # Set the parameters
+    parameters = {"date": date, "out": output, "logs": log_file,
+              "airflow": airflow}
+    
+    # Run the project
+    # info = mlflow.run('./', parameters=parameters, experiment_name=experiment)
+    info = mlflow.run('https://github.com/alexZeakis/MLFlow-GDELT-Downloading.git',
+                      parameters=parameters, experiment_name=experiment)
+    # info = mlflow.run('./', parameters=parameters, experiment_name=args.experiment, backend='docker')
     mlflow.end_run()
+    
+    with open(log_file) as o:
+        j = json.load(o)
+        mlflow.start_run(run_id=info.run_id)
+        for key, val in j.items():
+            mlflow.log_metric(key, val)
+        mlflow.end_run()    
 
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='An mlflow project for downloading \
+                                     GDELT articles from specific date.')
+    
+    parser.add_argument('-d', '--date', required=True, help='Date to download GDELT articles in the form of YYYYMMDD.')
+    
+    
+    parser.add_argument('-e', '--experiment', required=True, help='Experiment Name of MLFlow')
+    parser.add_argument('-a', '--airflow', help='Airflow run_id for connection')
+    parser.add_argument('-o', '--output', default="../data/", help='Path to the output file.')
+    parser.add_argument('-l', '--log', default="../logs/deduplicate/", help='Path to the log file.')
+    parser.add_argument('-t', '--tracking', help='URL of MLFlow tracking server')
+    parser.add_argument('-u', '--user', help='Name of MLFlow User.')
+    
+    args = parser.parse_args()
+    
+    parameters = {"date": args.date, 'experiment': args.experiment, 'airflow': args.airflow,
+                  'output': args.output, 'log': args.log, 'tracking': args.tracking, 'user': args.user}
+    
+    run(**parameters)
